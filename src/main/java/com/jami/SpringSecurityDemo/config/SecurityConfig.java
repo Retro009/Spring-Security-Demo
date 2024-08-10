419f0a096604869979dc71885f8292b7b6aa7b1b
Jeeshan Shaikh <jeeshannick@gmail.com>
2024-08-10 22:46:47 +0530

    Configured Password Encoder for Admin role

--- src/main/java/com/jami/SpringSecurityDemo/config/SecurityConfig.java
+++ src/main/java/com/jami/SpringSecurityDemo/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,7 +30,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$kXa5ZqBu8R1X8iPAKcloJeqKdQLBsA5CBSyHRF.A2y/qhghGR2kOm").authorities("admin").build(); //pw: 54321
         return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
